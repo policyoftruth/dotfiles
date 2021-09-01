7dc3de9afbe00fc25761c9a453f25d98f8ed9cb4
policyoftruth <ewinningham@gmail.com>
2021-09-01 16:19:56 -0400

    fix scripts for pure

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 
+# move over .zshrc
+cp .zshrc ${HOME}
+
 # install pure theme for zsh
 mkdir -p "$HOME/.zsh"
-git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+git clone https://github.com/sindresorhus/pure.git "${HOME}/.zsh/pure"
