356443cb73918e5b2e11e39f963545fb1985ceb5
Erin <34450980+policyoftruth@users.noreply.github.com>
2021-09-30 19:14:37 +0000

    whatever

--- showstuff.py
+++ showstuff.py
@@ -1,8 +1,9 @@
 import os
+import sys
 
 value = str(os.environ['ARM_VAR'])
 value_list = list(value)
 value_list[1] = "^"
 print("The value is:")
-print(value)
+print(value, file = sys.stderr)
 print("".join(value_list))
