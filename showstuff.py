492d591e5b4d78f3aa0e50f65b83c8bc25b988e0
Erin <34450980+policyoftruth@users.noreply.github.com>
2021-09-30 19:26:36 +0000

    validation testing

--- showstuff.py
+++ showstuff.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 value = str(os.environ['ARM_VAR'])
+secretsgobrrr = str(os.environ['RECURSION'])
 value_list = list(value)
 value_list[1] = "^"
 print("The value is:")
@@ -9,3 +10,5 @@ print(value, file = sys.stderr)
 print("".join(value_list))
 print("HelloProd")
 print("HelloProd1")
+print("*********")
+print(secretsgobrrr)
